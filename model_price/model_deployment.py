4925b4e3217653749c11a26712d14b22b1ff51c7
Juan Rave <juan.rave@gmail.com>
2023-04-30 15:51:23 -0500

    Update model_deployment.py

--- model_price/model_deployment.py
+++ model_price/model_deployment.py
@@ -5,9 +5,7 @@ import joblib
 import sys
 import os
 
-def predict(Year, Mileage, State, Make, Model):
-    
-     
+def predict(Year, Mileage, State, Make, Model):    
     xgb = joblib.load(os.path.dirname(__file__) + '/price.pkl') 
     data = {
     'Year':    Year, 
@@ -17,9 +15,7 @@ def predict(Year, Mileage, State, Make, Model):
     'Model':   Model
     }
     
-    df = pd.DataFrame([data])
-    
-    
+    df = pd.DataFrame([data])    
     df["State"] = df["State"].astype("category")
     df["Make"]  = df["Make"].astype("category")
     df["Model"] = df["Model"].astype("category")    
@@ -28,23 +24,17 @@ def predict(Year, Mileage, State, Make, Model):
 
     return price
 
-
-if __name__ == "__main__":
-    
+if __name__ == "__main__":    
     if len(sys.argv) == 1:
         print('Please add data')
-        
     else:
-
         Year      = sys.argv[1]
         Mileage   = sys.argv[2]
         State     = sys.argv[3]
         Make      = sys.argv[4]
         Model = sys.argv[5]
         
-
         p1 = predict(Year, Mileage, State, Make, Model)
         
         #print(url)
         print('price: ', p1)
-        
